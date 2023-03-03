229563ae263dd7ec866d384e6cb8d74b9361352f
shaileshkachare <shaileshkachare2@gmail.com>
2023-03-03 18:19:03 +0530

    Assignment solve

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,25 @@ import React from 'react'
 import '../styles/App.css';
 
 class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state={name: "John Doe", enrollmentNo: "12345678", age: 34}
+    }
   
+    handleButtonClick(){
+        this.setState({
+
+        })
+    }
+    render(){
+        return (
+            <>
+            <h1>{`Hello, my name is ${this.state.name}`}</h1>
+            <p>{`I am ${this.state.age} years old and my enrollment no is ${this.state.enrollmentNo}`}</p>
+            <button onClick={()=>this.handleButtonClick()}>Submit</button>
+            </>
+        )
+    }
 }
 
 
